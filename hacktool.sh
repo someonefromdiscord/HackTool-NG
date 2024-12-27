f4439acc40803a71cac8e07cf58e75798cbb8c8c
someonefromdiscord <freehostlvr12@proton.me>
2024-12-27 18:47:30 +0100

    Update hacktool.sh

--- hacktool.sh
+++ hacktool.sh
@@ -1,4 +1,4 @@
 read -p "Please enter the target's username." target
 echo "Hacking $target..."
-echo "$target has been successfully hacked! IP address of target: 127.0.0.1"
+echo "$target has been successfully hacked! IP address of target: $(curl myip.wtf/text), user agent: Error fetching information"
 echo "(This is a joke, don't take it too seriously)
