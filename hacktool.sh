b3c48e645836b1c9a886d6dee8aacf1fec164bf3
someonefromdiscord <freehostlvr12@proton.me>
2025-02-16 00:04:54 +0100

    Update hacktool.sh

--- hacktool.sh
+++ hacktool.sh
@@ -3,5 +3,8 @@ echo "------------"
 read -p "Please enter the target's username." target
 echo "Hacking $target..."
 sleep 1
+echo "Injecting malicious payload..."
+sleep 5
+echo "Installing 250 viruses on target's device..."
+sleep 10
 echo "$target has been successfully hacked! IP address of target: $(curl myip.wtf/text), user agent: Error fetching information"
-echo "(This is a joke, don't take it too seriously)
